aee227db574fdcfc52e8de59cf2a09da3682c9cc
aubreyalfonzo <aubrey.j.pohl@gmail.com>
2023-07-08 23:04:35 -0500

    yee haw wah eey

---
+++ js/collapse_func.js
@@ -0,0 +1,65 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    var activeCollapsible = document.querySelector(".collapsible.active");
+
+    if (activeCollapsible && activeCollapsible !== this) {
+      activeCollapsible.classList.toggle("active");
+      activeCollapsible.nextElementSibling.style.display = "none";
+      activeCollapsible.parentElement.style.width = "";
+      var img = activeCollapsible.querySelector(".plus");
+      var img2 = activeCollapsible.querySelector(".minus");
+      if (img) {
+        img.style.display = "block";
+        img2.style.display = "none";
+      }
+    }
+    
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      var img = this.querySelector(".plus");
+      var img2 = this.querySelector(".minus");
+      if (img) {
+        img.style.display = "block";
+        img2.style.display = "none";
+         this.parentElement.style.width = "";
+    }
+    } else {
+      content.style.display = "block";
+      var img = this.querySelector(".plus");
+      var img2 = this.querySelector(".minus");
+      if (img) {
+        img.style.display = "none";
+        img2.style.display = "block";
+        this.parentElement.style.width = "70vw";
+      }
+
+      if (width < 1200){
+        this.parentElement.style.width = "";
+      }
+    }
+
+  });
+}
+
+
+
+var coll = document.getElementsByClassName("collapsible2");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
