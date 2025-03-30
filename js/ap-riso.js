317a42142b7b3c8dde53c19aaf4621120009547e
aubreyalfonzo <aubrey.j.pohl@gmail.com>
2025-03-30 17:37:41 -0500

    file updates

--- js/ap-riso.js
+++ js/ap-riso.js
@@ -7,17 +7,38 @@ function ease(iVal, oVal, eVal){
   return oVal += (iVal - oVal) * eVal;
 }
 
-let libs = ['../js/includes/libs/p5.riso.js'];
+let libs = ['../includes/libs/p5.riso.js'];
 
 // let ditherType = 'floydsteinberg';
+
+let img;
+let imgs = [];
+let gif;
 
 let pink;
 let blue;
 let green;
 
+let layer;
+
+let x,y;
+
+let flightspeed;
+
+function preload(){
+	img = loadImage('../data/RISO/ME.png');
+	
+	for(let i=0; i<4; i++ ){
+		imgs[i] = loadImage('../data/RISO/' + i + '.png');
+	}
+	
+	gif = loadImage('../data/RISO/bird.gif');
+}
+
 function setup() {
 	background(255);
 	createCanvas(windowWidth, windowHeight);
+	layer = createGraphics(height/2, 0, WEBGL);
 	pixelDensity(1);
 	
 	pink = new Riso('fluorescentpink');
@@ -29,6 +50,15 @@ function setup() {
 	pink.fill(255);
 	blue.fill(255);
 	green.fill(255);
+	
+	textAlign(CENTER, CENTER);
+	
+	// img.resize(500, 500);
+	
+	x = random(width);
+	y = height;
+	
+	flightspeed = -3;
 }
 
 function draw() {
@@ -36,7 +66,28 @@ function draw() {
 	pink.background(255, 100);
 	blue.background(255, 100);
 	green.background(255, 100);
+
+	img.resize(height/2, 0);
 	
+	push();
+	imageMode(CENTER);
+	
+	x = x+flightspeed/2;
+	y = y+flightspeed;
+	
+	if(y == 0){
+		y = height;
+	}else{
+		if(x <= 0){
+			x = width;
+		}
+	}
+	// let dithered1 = ditherImage(img, ditherType, 50);
+	image(img, width/2, height/2);
+	image(gif, x, y);
+	// image(layer, width/2, height/2);
+	pop();
+
 	let shapesize = width/8;
 	
 	pink.ellipse(width/2, mouseY, shapesize);
@@ -51,6 +102,11 @@ function draw() {
 	blue.line(width, 0, mouseX, mouseY);
 	pink.line(width, height, mouseX, mouseY);
 	blue.line(0, height, mouseX, mouseY);
+	
+	text(mouseX+' x '+mouseY, mouseX, mouseY);
+	text(width/2+' x '+mouseY, width/2, mouseY);
+	text(mouseX+' x '+height/2, mouseX, height/2);
+
 	
 	
 	drawRiso();
