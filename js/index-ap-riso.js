e52592e1cc6340b6690cf45d65360506f2519a12
Aubrey <55700488+aubreyalfonzo@users.noreply.github.com>
2025-04-20 00:12:20 -0500

    Update index-ap-riso.js

--- js/index-ap-riso.js
+++ js/index-ap-riso.js
@@ -11,8 +11,8 @@ let libs = ['../js/includes/libs/p5.riso.js'];
 
 // let ditherType = 'floydsteinberg';
 
-let img;
-let gif;
+// let img;
+// let gif;
 
 let pink;
 let blue;
@@ -24,10 +24,10 @@ let x,y;
 
 let flightspeed;
 
-function preload(){
-	img = loadImage('https://raw.githubusercontent.com/aubreyalfonzo/apohl.xyz/refs/heads/main/data/assets/ME.png');
-	gif = loadImage('https://raw.githubusercontent.com/aubreyalfonzo/apohl.xyz/refs/heads/main/data/assets/bird.gif');
-}
+// function preload(){
+// 	img = loadImage('https://raw.githubusercontent.com/aubreyalfonzo/apohl.xyz/refs/heads/main/data/assets/ME.png');
+// 	gif = loadImage('https://raw.githubusercontent.com/aubreyalfonzo/apohl.xyz/refs/heads/main/data/assets/bird.gif');
+// }
 
 function setup() {
 	background(255);
@@ -60,26 +60,24 @@ function draw() {
 	blue.background(255, 100);
 	green.background(255, 100);
 
-	img.resize(height/2, 0);
+	// img.resize(height/2, 0);
 
-	push();
-	imageMode(CENTER);
+	// push();
+	// imageMode(CENTER);
 	
-	x = x+flightspeed/2;
-	y = y+flightspeed;
+	// x = x+flightspeed/2;
+	// y = y+flightspeed;
 	
-	if(y == 0){
-		y = height;
-	}else{
-		if(x <= 0){
-			x = width;
-		}
-	}
-	// let dithered1 = ditherImage(img, ditherType, 50);
-	image(img, width/2, height/2);
-	image(gif, x, y);
-	// image(layer, width/2, height/2);
-	pop();
+	// if(y == 0){
+	// 	y = height;
+	// }else{
+	// 	if(x <= 0){
+	// 		x = width;
+	// 	}
+	// }
+	// image(img, width/2, height/2);
+	// image(gif, x, y);
+	// pop();
 
 	let shapesize = width/8;
 	
